21910c7ef707a3d17c10ce0ddd3b90ff1469a675
Truong vi <vinlt@antsprogrammatic.com>
2020-06-27 06:37:46 +0700

    Deploy sandbox

--- Application/src/constant.js
+++ Application/src/constant.js
@@ -1,6 +1,6 @@
 export const appConfig = {
     API_GO: 'http://127.0.0.1:8001',
     SEXS: [{ id: 'male', value: 'Nam' }, { id: 'female', value: 'Nữ' }, { id: 'other', value: 'Khác' }],
-    API: 'https://vi.api.vinlt.wtf',
+    API: 'https://api-v2.vinlt.wtf',
     ACCESS_TOKEN_SECERET: 'access-token-secret-nltruongvi'
 };
